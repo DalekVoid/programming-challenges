4e35a58452f64e15c5b6b8f0b10692a80b72755c
Dalek Void <dalek@voidamplifier.com>
2015-01-13 16:51:11 +0800

    Completed all getting-started challenges, need polish up later on

--- getting-started/graphical-editor/c++/main.cpp
+++ getting-started/graphical-editor/c++/main.cpp
@@ -1,69 +1,183 @@
 #include<iostream>
+#include<vector>
+#include<sstream>
 using namespace std;
 
-struct image{
-  unsigned int width;
-  unsigned int height;
-  char *canvas;
+class Image{
+public:
+  Image(unsigned int width, unsigned int height){
+    if (width>0 && height>0){
+      for (int i = 0; i < height; i++){
+        this->canvas.push_back(vector<char>(width, 'O'));
+      }
+    }
+  }
+
+  ~Image(){
+  }
+
+  // convenience methods
+  unsigned int width() const{
+    return this->canvas[0].size();
+  }
+
+  unsigned int height() const{
+    return this->canvas.size();
+  }
+
+  bool checkHeightBound(int value) const{
+    return value >= 0 && value < height();
+  }
+
+  bool checkWidthBound(int value) const{
+    return value >= 0 && value < width();
+  }
+
+  void paint(int x, int y, char color){
+    if(checkHeightBound(y) && checkWidthBound(x))
+      canvas[y][x] = color;
+  }
+  
+  void fill(int x, int y, char color){
+    if(checkHeightBound(y) && checkWidthBound(x))
+      fillImp(x, y, canvas[y][x], color);
+  }
+
+  void clear(){
+    for(unsigned j = 0; j < height(); j++){
+      for(unsigned i = 0; i < width(); i++){
+        canvas[j][i] = 'O';
+      }
+    }
+  }
+
+friend ostream& operator<< (ostream& os, const Image& img);
+
+private:
+  vector< vector<char> > canvas;
+
+  void fillImp(int x, int y, char fromColor, char toColor){
+    if (checkHeightBound(y) && checkWidthBound(x) && canvas[y][x] == fromColor && fromColor != toColor){
+      canvas[y][x] = toColor;
+      fillImp(x-1, y, fromColor, toColor);
+      fillImp(x, y-1, fromColor, toColor);
+      fillImp(x+1, y, fromColor, toColor);
+      fillImp(x, y+1, fromColor, toColor);
+    }
+  }
 };
 
-bool checkBound(int value, int limit){
-  return value >=1 && value <= limit;
-}
-
-void paint(char* image, int width, int height, int x, int y, char color){
-  if(checkBound(x, width) && checkBound(y, height))
-    *(image + y*width + x) = color;
-}
- 
-void print(char* image, int width, int height){
-  for(unsigned j = 0; j < height; j++){
-    for(unsigned i = 0; i < width; i++){
-      cout << *(image + j*width + width);
+ostream& operator<< (ostream& os, const Image& img){
+  if (img.canvas.size() != 0){
+    for(unsigned j = 0; j < img.height(); j++){
+      for(unsigned i = 0; i < img.width(); i++){
+        os << img.canvas[j][i]; 
+      }
+      os << endl;
     }
-    cout << endl;
   }
+  return os;
 }
-
-void clear(image img){
-  if (img.canvas != NULL){
-    memset((void*)img.canvas, 'O', img.width*img.height);
-  }
-}
-
 
 int main(int argc, char** argv){
-  char *image;
-  char command;
+  Image* img = NULL;
+  char code;
   char color;
   unsigned int width, height;
-  unsigned int x, y;
+  unsigned int x, y, x1, x2, y1, y2;
   bool terminate = false;
+  string name;
 
-  while(!terminate && cin >> command){
-    switch(command){
+  istringstream iss;
+  string command;
+  while(!terminate){
+    getline(cin, command);
+    iss.clear();
+    iss.str(command);
+
+    iss >> code;
+    switch(code){
       case 'I':
-        cin >> width >> height;
-        image = new char[width*height];
-        memset((void*)image, 'O', width*height);
+        iss >> width >> height;
+        if (img != NULL)
+          delete img;
+        img = new Image(width, height);
         break;
       case 'C':
-
+        if (img != NULL)
+          img->clear();
+        break;
       case 'L':
-        cin >> x >> y >> color;
-        paint(image, width, height, x, y, color);
+        iss >> x >> y >> color;
+        if (img != NULL)
+          img->paint(x-1, y-1, color);
         break;
       case 'V':
+        iss >> x >> y1 >> y2 >> color;
+        if (img != NULL){
+          for (int y = y1;;){
+            img->paint(x-1, y-1, color);
+            if (y == y2)
+              break;
+            else if (y1 < y2)
+              y++;
+            else
+              y--;
+          }
+        }
         break;
       case 'H':
+        iss >> x1 >> x2 >> y >> color;
+        if (img != NULL){
+          for (int x = x1;;){
+            img->paint(x-1, y-1, color);
+            if (x == x2)
+              break;
+            else if (x1 < x2)
+              x++;
+            else
+              x--;
+          }
+        }
         break;
       case 'K':
+        iss >> x1 >> y1 >> x2 >> y2 >> color;
+        if (img != NULL){
+          for (int x = x1;;){
+            for (int y = y1;;){
+              img->paint(x-1, y-1, color);
+              if (y == y2)
+                break;
+              else if (y1 < y2)
+                y++;
+              else
+                y--;
+            }
+            if (x == x2)
+              break;
+            else if (x1 < x2)
+              x++;
+            else
+              x--;
+          }
+        }
         break;
       case 'F':
+        iss >> x >> y >> color;
+        if (img != NULL){
+          img->fill(x-1, y-1, color);
+        }
         break;
       case 'S':
+        iss >> name;
+        if (img != NULL){
+          cout << name << endl;
+          cout << *img;
+        }
         break;
       case 'X':
+        if (img != NULL)
+          delete img;
         terminate = true;
         break;
       default:
